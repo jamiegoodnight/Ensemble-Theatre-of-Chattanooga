f568436a9cc879508b8f42d6953ae6260f4b03fc
Jamie Goodnight <goodnightlambda@gmail.com>
2019-12-30 03:06:22 -0500

    :lipstick: Beginning styling for shows and titles

--- src/components/HomeCTA.js
+++ src/components/HomeCTA.js
@@ -4,64 +4,59 @@ const HomeCTA = () => {
   return (
     <div className='home-wrapper'>
       <div className='cta'>
-        {/* --- */}
         <div className='logo-wrapper'></div>
-        {/* --- */}
-        <div className='divider'></div>
-        {/* --- */}
-        <div className='events-wrapper'>
-          <div className='event-wrapper'>
-            <div className='event-date'>
-              <h3>THU</h3>
-              <div className='event-divider'></div>
-              <h3>FEB 6</h3>
-            </div>
-            <div className='event-info'>
-              <h2 className='info-heading'>Assassins</h2>
-              <h4 className='info-subheading'>
-                by Stephen Sondheim and John Weidman
-              </h4>
-              <p>7:15 at Barking Legs</p>
-            </div>
-            {/* <div className='event-image'></div> */}
-          </div>
-          <div className='event-wrapper'>
-            <div className='event-date'>
-              <h3>THU</h3>
-              <div className='event-divider'></div>
-              <h3>FEB 6</h3>
-            </div>
-            <div className='event-info'>
-              <h2 className='info-heading'>Assassins</h2>
-              <h4 className='info-subheading'>
-                by Stephen Sondheim and John Weidman
-              </h4>
-              <p>7:15 at Barking Legs</p>
-            </div>
-            {/* <div className='event-image'></div> */}
-          </div>
-          <div className='event-wrapper'>
-            <div className='event-date'>
-              <h3>THU</h3>
-              <div className='event-divider'></div>
-              <h3>FEB 6</h3>
-            </div>
-            <div className='event-info'>
-              <h2 className='info-heading'>Assassins</h2>
-              <h4 className='info-subheading'>
-                by Stephen Sondheim and John Weidman
-              </h4>
-              <p>7:15 at Barking Legs</p>
-            </div>{' '}
-            {/* <div className='event-image'></div> */}
-          </div>
-          {/* --- */}
-        </div>
-        {/* --- */}
       </div>
-      {/* --- */}
     </div>
   );
 };
 
 export default HomeCTA;
+
+// {/* <div className='events-wrapper'>
+// <div className='event-wrapper'>
+//   <div className='event-date'>
+//     <h3>THU</h3>
+//     <div className='event-divider'></div>
+//     <h3>FEB 6</h3>
+//   </div>
+//   <div className='event-info'>
+//     <h2 className='info-heading'>Assassins</h2>
+//     <h4 className='info-subheading'>
+//       by Stephen Sondheim and John Weidman
+//     </h4>
+//     <p>7:15 at Barking Legs</p>
+//   </div>
+//   {/* <div className='event-image'></div> */}
+// </div>
+// <div className='event-wrapper'>
+//   <div className='event-date'>
+//     <h3>THU</h3>
+//     <div className='event-divider'></div>
+//     <h3>FEB 6</h3>
+//   </div>
+//   <div className='event-info'>
+//     <h2 className='info-heading'>Assassins</h2>
+//     <h4 className='info-subheading'>
+//       by Stephen Sondheim and John Weidman
+//     </h4>
+//     <p>7:15 at Barking Legs</p>
+//   </div>
+//   {/* <div className='event-image'></div> */}
+// </div>
+// <div className='event-wrapper'>
+//   <div className='event-date'>
+//     <h3>THU</h3>
+//     <div className='event-divider'></div>
+//     <h3>FEB 6</h3>
+//   </div>
+//   <div className='event-info'>
+//     <h2 className='info-heading'>Assassins</h2>
+//     <h4 className='info-subheading'>
+//       by Stephen Sondheim and John Weidman
+//     </h4>
+//     <p>7:15 at Barking Legs</p>
+//   </div>{' '}
+//   {/* <div className='event-image'></div> */}
+// </div>
+// {/* --- */}
+// </div> */}
--- src/components/HomeShows.js
+++ src/components/HomeShows.js
@@ -5,10 +5,16 @@ const HomeShows = () => {
     <div className='homeshows-wrapper'>
       <div className='row'>
         <div className='show-wrapper-one'>
-          <div className='show-one'></div>
+          <div className='show-one'>
+            <p>Jeremy Wilkins as Caliban</p>
+            <h3>THE TEMPEST</h3>
+          </div>
         </div>
         <div className='show-wrapper-two'>
-          <div className='show-two'></div>
+          <div className='show-two'>
+            <p>Joey Tipton as Katurian</p>
+            <h3>THE PILLOWMAN</h3>
+          </div>
         </div>
       </div>
       <div className='row'>
@@ -16,15 +22,31 @@ const HomeShows = () => {
           <div className='show-three'></div>
         </div>
         <div className='show-wrapper-four'>
-          <div className='show-four'></div>
+          <div className='show-four'>
+            <div className='four-box'>
+              <h3>THE</h3>
+              <h3>SCARLET</h3>
+              <h3>LETTER</h3>
+            </div>
+            <p>Apryl Kelli Hughes as Hester Prynne</p>
+          </div>
         </div>
       </div>
       <div className='row'>
         <div className='show-wrapper-five'>
-          <div className='show-five'></div>
+          <div className='show-five'>
+            <h3>OTHELLO</h3>
+            <p>E'tienne Easley as Othello</p>
+          </div>
         </div>
         <div className='show-wrapper-six'>
-          <div className='show-six'></div>
+          <div className='show-six'>
+            <div className='six-box'>
+              <p>Christy Tipton as Keely</p>
+              <p>Kitty Reel as Du</p>
+              <h3>KEELY AND DU</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
