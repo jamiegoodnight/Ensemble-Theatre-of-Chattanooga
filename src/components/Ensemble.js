8e1d588c293ef91ab8c1c2b0b31e8186d720430b
eawyatt <red@ensembletheatreofchattanooga.com>
2020-01-16 01:44:39 -0500

    Added one ensemble. Will do more tomorrow.

--- src/components/Ensemble.js
+++ src/components/Ensemble.js
@@ -7,11 +7,15 @@ const Ensemble=()=>{
         <div className='Ensemble-Wrapper'>
             <div className='Purple'></div>
             <div className='Red'>
-                <div className= 'Blue EWyatt'></div>
+            <div className= 'Blue KatieWarren'></div>
+            <div className= 'Black'>
+                    <h3>Katie Warren</h3>
+                    <p>Katie Warren is very excited to be a part of "Bridges of Madison County" and "Loving, Repeating" for the Ensemble Theater of Chattanooga. Previous roles she has performed include Buttons in "Lying in State", Miss Hannigan in "Annie", and Despina in Mozart's "Cosi Fan Tutte". When she is not on the stage, she is in the classroom, spreading her love of music to her students as an elementary music teacher. She has a passion for theater, and looks forward to what this year will bring.</p>
+                </div>
+                <div className= 'Blue RedWyatt'></div>
                 <div className= 'Black'>
-                    <h3>Red</h3>
-                    <p>BIO HERE!</p>
-                    <i class="far fa-address-card"></i>
+                    <h3>Eric "Red" Wyatt</h3>
+                    <p>Bio will go here in due time</p>
                 </div>
                 <div className= 'Black'>
                     <h3>Red</h3>
