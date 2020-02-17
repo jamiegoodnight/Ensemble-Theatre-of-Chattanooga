a5eaec09ead209f4396e2e01e97d0da53e829708
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-02-16 23:18:41 -0500

    :construction: Changing wording

--- src/components/Now.js
+++ src/components/Now.js
@@ -8,7 +8,7 @@ const Now = () => {
       <div className='now-img'></div>
       <div className='now-text'>
         <h2>ASSASSINS</h2>
-        <h3>by Stephen Sondheim & John Weidman</h3>
+        <h3>by Stephen Sondheim and John Weidman</h3>
         <p>Kicking off its 2020 season, the Ensemble Theatre of Chattanooga will present the musical ASSASSINS by Stephen Sondheim and John Weidman.  This politically charged concept musical takes both a historical and philosophical look at the nine presidential assassins, imagining a world where they intermingle and discuss their significance.</p>
         <p>James Ogden will make his directorial debut with Ensemble Theatre of Chattanooga having appeared last year in SIDE SHOW and THE CHRISTIANS.  The rest of the cast includes newcomers Andrew Steele, Josh Garcia, Christian Smith, Gabriel Hubbard, Zack Caywood, Austin Campbell and Michael Myers.  They are joined by ETC veterans Jordan Bennet, Alyssa Whitesell, and Cody Keown. Music Direction is provided by Terry Sanford and Stage Manager is Joel Sanchez-Avantes.</p>
       </div>
