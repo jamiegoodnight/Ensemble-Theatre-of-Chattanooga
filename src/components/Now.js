2a1eba48182bc3a8c465706b0bff2c77e916fda6
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-02-29 12:04:59 -0500

    :construction: Removed Frozen cast info

--- src/components/Now.js
+++ src/components/Now.js
@@ -11,7 +11,7 @@ const Now = () => {
         <h2>FROZEN</h2>
         <h3>by Bryony Lavery</h3>
         <p>The Ensemble Theatre of Chattanooga next presents the award-winning play FROZEN by Bryony Lavery. "One evening ten-year old Rhona goes missing. As her mother retreats into a state of frozen hope, a psychologist studies the brain of a serial killer to find out if what he does is pure evil or simply beyond his control. Drawn together by horrific circumstances, these three embark on a long, dark journey that ends in the discovery of a common humanity." </p>
-        <p>Directed by Ensemble Theatre of Chattanooga veteran Bruce Shaw. The cast includes Casey Keelen and Eric "Red" Wyatt. Don't forget to follow us on Facebook, Twitter, and Instagram for more updates.</p>
+        <p>Directed by Ensemble Theatre of Chattanooga veteran Bruce Shaw. Don't forget to follow us on Facebook, Twitter, and Instagram for updates on the cast, crew, and more.</p>
       </div>
     </div>
     <div className='divider'></div>
